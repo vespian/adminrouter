cdc1467167c80b329d13437704e454d2ae65ea92
Pawel Rozlach <prozlach@mesosphere.io>
2016-11-10 01:15:58 +0100

    wip

--- lib/shim.lua
+++ lib/shim.lua
@@ -1,4 +1,4 @@
-local util = require "common.util"
+local util = require "common.util";
 local agent = require 'master.agent.common';
 
 local shim = {}
@@ -13,51 +13,51 @@ if use_auth ~= "true" then
         )
     auth.validate_jwt_or_exit = function() return end
 else
-    ngx.log(ngx.NOTICE, "Use auth module.")
-    auth = require "lib.auth.ee"
+    ngx.log(ngx.NOTICE, "Use auth module.");
+    auth = require "lib.auth.ee";
 end
 
-local function shim.request(url)
+function shim.request(url)
     return util.request(url, {});
 end
 
-local function shim.access_agent_endpoint()
+function shim.access_agent_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.set_agent_addr()
+function shim.set_agent_addr()
     return agent.set_agent_addr("http://");
 end
 
-local function shim.access_acsapi_endpoint()
+function shim.access_acsapi_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_lashupkey_endpoint()
+function shim.access_lashupkey_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_service_endpoint()
+function shim.access_service_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_metadata_endpoint()
+function shim.access_metadata_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_historyservice_endpoint()
+function shim.access_historyservice_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_mesosdns_endpoint()
+function shim.access_mesosdns_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_systemhealth_endpoint()
+function shim.access_systemhealth_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
-local function shim.access_pkgpanda_endpoint()
+function shim.access_pkgpanda_endpoint()
     return auth.validate_jwt_or_exit()
 end
 
